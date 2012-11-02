40af8aabee483819ce0ccf7f33929b7af9a6951c
marcosifpe <marcosifpe@68d90fe1-f498-3002-3d8f-6b7629146aad>
2012-11-02 23:47:14 +0000

    Questão 20.

--- L1C/q20/q20.c
+++ L1C/q20/q20.c
@@ -2,12 +2,41 @@
  * q20.c
  *
  *  Created on: 02/11/2012
- *      Author: masc
+ *      Author: Marcos Costa
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stddef.h>
+
+#define T 30
 
 int main(void) {
-	printf("teste");
+	int i, n, v[T], soma;
+	freopen("e20.txt", "r", stdin);
+	freopen("s20.txt", "w", stdout);
+	scanf("%d", &n);
+
+	srand(time(NULL));
+
+	while (n--) {
+		for (i = 0, soma = 0; i < T; ++i) {
+			v[i] = rand() % 100 + 1;
+			soma += v[i];
+		}
+
+		printf("%d = ", soma);
+		for (i = 0; i < T; ++i) {
+			 printf("%d ", v[i]);
+			 if (i != T - 1)
+				 printf("+ ");
+			 else
+				 printf("\n");
+		}
+	}
+
+	fclose(stdin);
+	fclose(stdout);
 	return 0;
 }
 
