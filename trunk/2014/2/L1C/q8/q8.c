c220ce450cf3b71f61b1ccbb8ab9aa0dc7f91307
edmilsonsantana82@gmail.com <edmilsonsantana82@gmail.com@68d90fe1-f498-3002-3d8f-6b7629146aad>
2014-11-29 13:13:01 +0000

    Questão q8 modificada

--- trunk/2014/2/L1C/q8/q8.c
+++ trunk/2014/2/L1C/q8/q8.c
@@ -8,15 +8,22 @@
 #include <stdlib.h>
 
 int main(void) {
-	int raio, a, l, p;
+	int raio, a, l, p, validaTamanho, diametro;
 	freopen("e8.txt", "r", stdin);
 	freopen("s8.txt", "w", stdout);
 	scanf("%d", &raio);
 
 	while (raio) {
+		diametro = 2*raio;
+		validaTamanho = 0;
+
 		scanf("%d %d %d", &a, &l, &p);
 
-		if (a * l * p >= raio * 2)
+		validaTamanho += (diametro <= a);
+		validaTamanho += (diametro <= l);
+		validaTamanho += (diametro <= p);
+
+		if (validaTamanho == 3)
 			printf("S\n");
 		else
 			printf("N\n");
